f47d47f97d910e18f2d6614d1e88d616531346a3
lb_ubuntu <lbruestl1@stud.hs-offenburg.de>
2026-08-17 18:32:50 +0200

    refactor entity stores

--- src/domain/common/EntityStores.ts
+++ src/domain/common/EntityStores.ts
@@ -1,14 +1,68 @@
-// import { Node, Edge, Message } from "./Entities";
-// import { IdentifiableStore } from "./Identifiables";
+import { GenericNode } from "../algorithm/data/AlgoData.js";
 
-// export class NodeStore<NodeType extends Node>
-//     extends IdentifiableStore<NodeType> {
-// }
+interface MyIdentifiable {
+    id: number,
+}
 
-// export class EdgeStore<EdgeType extends Edge>
-//     extends IdentifiableStore<EdgeType> {
-// }
+export class IdentifiableStoreError extends Error { }
 
-// // export class MessageStore<MessageType extends Message>
-// //     extends IdentifiableStore<MessageType> {
-// // }
+/**
+ * Handles storing and retrieving of data that 
+ * satisfies the interface Identifiable
+ * while only allowing the data to be of the same type.
+ */
+export class IdentifiableStore<T extends MyIdentifiable> {
+
+    private map: Map<number, T> = new Map();
+
+    /**
+     * Inserts item
+     * @param target 
+     * @throws IdentifiableStoreError if item with given id already exists
+     */
+    public insert(target: T): void {
+        if (this.map.has(target.id)) {
+            throw new IdentifiableStoreError(
+                `Item with id ${target.id} already exists in map.
+                Insertion failed for target ${target}`
+            );
+        }
+        this.map.set(target.id, target);
+    }
+
+    /**
+     * Updates stored item by setting it to target
+     * @param target 
+     * @throws IdentifiableStoreError if no item with given id exists
+     */
+    public update(target: T): void {
+        if (!this.map.has(target.id)) {
+            throw new IdentifiableStoreError(
+                `Item with id ${target.id} does not exist in map.
+                Update failed for target ${target}`
+            );
+        }
+        this.map.set(target.id, target);
+    }
+
+    /**
+     * Returns item with given id
+     * @param target 
+     * @throws IdentifiableStoreError if item with given id does not exist
+     */
+    public peek(target: MyIdentifiable): T {
+        const res: T | undefined = this.map.get(target.id);
+        if (res === undefined) {
+            throw new IdentifiableStoreError(
+                `Item with id ${target.id} does not exist in map.
+                Peeking failed for target ${target}`
+            );
+        }
+        return res;
+    }
+
+}
+
+
+export type GenericNodeStore = IdentifiableStore<GenericNode>
+export type GenericEdgeStore = IdentifiableStore<GenericNode>
--- src/domain/common/Identifiables.ts
+++ src/domain/common/Identifiables.ts
@@ -1,9 +1,6 @@
 // import TsMap from "ts-map";
 
 
-
-
-
 // export abstract class IIdManager {
 
 //     public abstract isValidNewId(id: number): boolean;
@@ -13,71 +10,9 @@
 // }
 
 
-
-// export abstract class IIdentifiableManager {
-
-//     public abstract insertIdentifiable(item: Identifiable): boolean;
-
-//     public abstract peakItem(id: number): Readonly<Identifiable> | null;
-
-//     public abstract popItem(id: number): Identifiable | null;
-
-// }
-
-
-
 // export abstract class IdentifiableStore<IdentifiableType extends Identifiable>
 //     implements
-//     IIdManager,
-//     IIdentifiableManager {
-
-//     //* ----------------------------------------
-//     //* IIdentifiableManager Impl
-
-//     private items: TsMap<number, IdentifiableType> = new TsMap();
-
-//     // inserts item 
-//     // if id is valid
-//     public insertIdentifiable(item: IdentifiableType): boolean {
-//         if (!this.isValidNewId(item.id)) {
-//             return false;
-//         }
-
-//         // insert
-//         this.items.set(item.id, item);
-
-//         // update nextItemID
-//         if (item.id >= this.nextItemID) {
-//             this.nextItemID = item.id + 1;
-//         }
-//         return true;
-//     }
-
-//     // returns readonly accessor for item
-//     // if item with given id exists
-//     public peakItem(id: number): Readonly<IdentifiableType> | null {
-//         const item = this.items.get(id);
-//         if (item !== undefined) {
-//             return item;
-//         }
-//         return null;
-//     }
-
-//     // returns item
-//     // after removing it from internal items
-//     // if id is valid
-//     public popItem(id: number): IdentifiableType | null {
-//         const item = this.items.get(id);
-//         if (item !== undefined) {
-//             this.items.delete(id);
-//             return item;
-//         }
-//         return null;
-//     }
-
-
-//     //* ----------------------------------------
-//     //* IInternalIDManager Impl
+//     IIdManager {
 
 //     private nextItemID = 0;
 
