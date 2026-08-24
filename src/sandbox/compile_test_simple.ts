f73ba16f4ec5453b85529cac2c2e859c56fe6423
lb_ubuntu <lbruestl1@stud.hs-offenburg.de>
2026-08-24 19:44:15 +0200

    use new engine in sandbox

--- src/sandbox/compile_test_simple.ts
+++ src/sandbox/compile_test_simple.ts
@@ -5,10 +5,14 @@ import { NodeProcessLog } from "../../src/domain_layer/components/algorithm_plug
 import { MessageState } from "../../src/domain_layer/components/algorithm_plugins/api/entities/state_entities/Messages.js";
 import { EchoAlgorithmNodeState } from "../../src/domain_layer/components/algorithm_plugins/plugins/echo_algorithm/EchoAlgoEntities.js";
 import { PendingMessage, SimulationSnapshot } from "../../src/domain_layer/components/simulation/SimulationSnapshot.js";
-import { ISimulationEngine, SimEng } from "../../src/domain_layer/components/simulation/engine/SimulationEngine.js";
 import { EchoAlgorithmNodeProcess } from "../../src/domain_layer/components/algorithm_plugins/plugins/echo_algorithm/EchoAlgoNodeProcess.js";
 import { SnapshotDataWorker } from "../domain_layer/components/simulation/worker/SnapshotWorker.js";
+import { EntityStateObserver, MessageStateObserver, NodeProcessLogObserver, NodeStateObserver } from "../domain_layer/components/simulation/presenter/SimSnapshotObserver.js";
+import { NodeState } from "../domain_layer/components/algorithm_plugins/api/entities/state_entities/Nodes.js";
+import { ISimulationEngine, SimulationEngine } from "../domain_layer/components/simulation/engine/SimulationEngine.js";
 
+
+// Setup Simulation Context
 const logStore = new IndexedStore<NodeProcessLog>();
 const nodeStore = new IndexedStore<EchoAlgorithmNodeState>();
 const edgeStore = new IndexedStore<BiDirectionalEdgeState>();
@@ -24,8 +28,7 @@ const snapshot = new SimulationSnapshot(
     logStore, nodeStore, edgeStore, msgStore, pendingMsgs, 0
 );
 
-
-// Setup Simulation Context
+// Init Simulation Context
 nodeStore.insert(new EchoAlgorithmNodeState(0, false, false, 0, null));
 nodeStore.insert(new EchoAlgorithmNodeState(1, false, false, 0, null));
 nodeStore.insert(new EchoAlgorithmNodeState(2, false, false, 0, null));
@@ -39,19 +42,25 @@ edgeStore.insert(new BiDirectionalEdgeState(3, { id: 2 }, { id: 4 }, 130));
 edgeStore.insert(new BiDirectionalEdgeState(4, { id: 1 }, { id: 4 }, 140));
 
 
+//? Dummy Setup Entity Observer
+const updates: Set<number> = new Set<number>();
+const obsLogs: NodeProcessLogObserver = new EntityStateObserver<NodeProcessLog>(updates);
+const obsNodes: NodeStateObserver = new EntityStateObserver<NodeState>(updates);
+const obsMsgs: MessageStateObserver = new EntityStateObserver<MessageState>(updates);
+
 // Setup SimulationEngine
-const engine: ISimulationEngine = new SimEng<EchoAlgorithmNodeState>(
-    snapshot, new EchoAlgorithmNodeProcess(), new SnapshotDataWorker()
+const engine: ISimulationEngine = new SimulationEngine<EchoAlgorithmNodeState>(
+    snapshot,
+    new SnapshotDataWorker(), new EchoAlgorithmNodeProcess(),
+    obsLogs, obsNodes, obsMsgs
 );
 
+//? Dummy Execution
+engine.simulateInitiation(0);
+engine.simulateTimeAdvancement(0);
 
-// Init Simulation
-engine.handleInitiation(0);
-
-// Step Simulation
 while (snapshot.pendingMessages.length > 0) {
-    snapshot.simulationTimestamp += 25;
-    engine.handlePendingMessages();
+    engine.simulateTimeAdvancement(25);
 }
 
 console.log(snapshot.nodeStates);
