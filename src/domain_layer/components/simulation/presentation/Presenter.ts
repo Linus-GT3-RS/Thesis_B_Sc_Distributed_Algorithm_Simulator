af32dfd8ef59f531ca28a17acb6435480a739d12
lb_ubuntu <lbruestl1@stud.hs-offenburg.de>
2026-09-06 19:42:03 +0200

    impl Presenter EdgeStates and MessageStates

--- src/domain_layer/components/simulation/presentation/Presenter.ts
+++ src/domain_layer/components/simulation/presentation/Presenter.ts
@@ -1,10 +1,12 @@
 import { Identifiable, ReadonlyIndexedStore } from "../../../../common/EntityStores.js";
 import { IDomainEventGateway } from "../../../gateways/EventGateway.js";
-import { NodeLogUpdatedEv, UpdatedNodeLogEv, UpdatedNodeStateEv } from "../../../gateways/Events.js";
+import { NodeLogUpdatedEv, UpdatedEdgeStateEv, UpdatedMessageStateEv, UpdatedNodeLogEv, UpdatedNodeStateEv } from "../../../gateways/Events.js";
+import { BiDirectionalEdgeState } from "../../algorithm_plugins/api/entities/state_entities/Edges.js";
 import { NodeLog } from "../../algorithm_plugins/api/entities/state_entities/Logs.js";
+import { MessageState } from "../../algorithm_plugins/api/entities/state_entities/Messages.js";
 import { NodeState } from "../../algorithm_plugins/api/entities/state_entities/Nodes.js";
-import { ModelBuilderNodeLog, ModelBuilderNodeState } from "./models/PresentationModelBuilder.js";
-import { PresentationModelNodeLog, PresentationModelNodeState } from "./models/PresentationModels.js";
+import { ModelBuilderEdgeState, ModelBuilderMessageState, ModelBuilderNodeLog, ModelBuilderNodeState } from "./models/PresentationModelBuilder.js";
+import { PresentationModelEdgeState, PresentationModelMessageState, PresentationModelNodeLog, PresentationModelNodeState } from "./models/PresentationModels.js";
 
 //* Interface
 
@@ -82,6 +84,50 @@ export class PresenterNodeStates implements IEntityPresenter {
         const model: PresentationModelNodeState = this.modelBuilder.build(updtEnt);
 
         this.eventGateway.emit(new UpdatedNodeStateEv(model));
+    }
+
+}
+
+
+export class PresenterMessageStates implements IEntityPresenter {
+
+    constructor(
+        private store: ReadonlyIndexedStore<MessageState>,
+        private modelBuilder: ModelBuilderMessageState,
+        private eventGateway: IDomainEventGateway,
+    ) { }
+
+
+    public presentUpdated(id: number): void {
+        const updtEntity: Readonly<MessageState> =
+            this.store.read({ id: id });
+
+        const model: PresentationModelMessageState =
+            this.modelBuilder.build(updtEntity);
+
+        this.eventGateway.emit(new UpdatedMessageStateEv(model));
+    }
+
+}
+
+
+
+export class PresenterEdgeStates implements IEntityPresenter {
+
+    constructor(
+        private store: ReadonlyIndexedStore<BiDirectionalEdgeState>,
+        private modelBuilder: ModelBuilderEdgeState,
+        private eventGateway: IDomainEventGateway,
+    ) { }
+
+    public presentUpdated(id: number): void {
+        const updtEntity: Readonly<BiDirectionalEdgeState> =
+            this.store.read({ id: id });
+
+        const model: PresentationModelEdgeState =
+            this.modelBuilder.build(updtEntity);
+
+        this.eventGateway.emit(new UpdatedEdgeStateEv(model));
     }
 
 }
